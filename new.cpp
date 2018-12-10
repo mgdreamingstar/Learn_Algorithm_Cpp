88450d4ecbfcc48ccb08ffce3c814a1f1f03d226
mgdreamingstar <guorch@buaa.edu.cn>
2018-12-10 22:53:53 +0800

    试着debug c++代码

--- new.cpp
+++ new.cpp
@@ -1,10 +1,10 @@
-#include <stdio.h>
 #include <iostream>
+#include <stdio.h>
 
 int main()
 {
-    printf("%0.1f\n",1.0/2);
-    printf("the answer is: %d, 2+3")
+    printf("%0.1f\n", 1.0 / 2);
+    printf("the answer is: %d\n", 2 + 3);
     system("pause");
     return 0;
 }
