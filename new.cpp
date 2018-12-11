c8ddfa9540be8b3c16e320bbf9d3e2f154eac556
mgdreamingstar <guorch@buaa.edu.cn>
2018-12-11 11:48:45 +0800

    解决 include 问题。

--- new.cpp
+++ new.cpp
@@ -3,8 +3,11 @@
 
 int main()
 {
+    int a, b;
     printf("%0.1f\n", 1.0 / 2);
     printf("the answer is: %d\n", 2 + 3);
+    scanf("%d%d", &a, &b);
+    printf("%d\n", a + b);
     system("pause");
     return 0;
 }
