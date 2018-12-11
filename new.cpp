afc758f440fa12114260bdaef181354b4321f702
mgdreamingstar <guorch@buaa.edu.cn>
2018-12-11 18:26:41 +0800

    第一章最后的实验。

--- new.cpp
+++ new.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
     int a, b;
     printf("%0.1f\n", 1.0 / 2);
     printf("the answer is: %d\n", 2 + 3);
-    scanf("%d%d", &a, &b);
-    printf("%d\n", a + b);
+    // scanf("%d%d", &a, &b);
+    // printf("%d\n", a + b);
+    // printf("%f\n", 11111.0 * 11111);
+    // printf("%f\n", 111111.0 * 111111);
+    // printf("%f\n", 1111111.0 * 1111111);
+    printf("%d\n", sqrt(-10));
     system("pause");
     return 0;
 }
